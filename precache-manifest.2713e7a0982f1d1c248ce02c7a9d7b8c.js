8111d26f2b00479ac2f2e8a9e8999dba695803a5
Lasisi <56618398+kenny15335@users.noreply.github.com>
2020-10-05 22:21:44 +0100

    Updates

---
+++ precache-manifest.2713e7a0982f1d1c248ce02c7a9d7b8c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcf0f4be32542f14800ab3d7ff99012",
+    "url": "/template.git/index.html"
+  },
+  {
+    "revision": "b19a9611f6c67a3d66d5",
+    "url": "/template.git/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "4dfc65c0a64dfc521542",
+    "url": "/template.git/static/css/main.b71468d8.chunk.css"
+  },
+  {
+    "revision": "b19a9611f6c67a3d66d5",
+    "url": "/template.git/static/js/2.04759c07.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/template.git/static/js/2.04759c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dfc65c0a64dfc521542",
+    "url": "/template.git/static/js/main.cc8e4815.chunk.js"
+  },
+  {
+    "revision": "12d4dc9509ac782804b0",
+    "url": "/template.git/static/js/runtime-main.00951df2.js"
+  },
+  {
+    "revision": "5a4547527e2da568956d065f97b1ad70",
+    "url": "/template.git/static/media/crypto.5a454752.png"
+  },
+  {
+    "revision": "4ba43602d2b6180011f7bb06c59dd044",
+    "url": "/template.git/static/media/loader.4ba43602.svg"
+  },
+  {
+    "revision": "1044a68b576ac1e2bb5b4609e1b594b7",
+    "url": "/template.git/static/media/svg-1.1044a68b.svg"
+  },
+  {
+    "revision": "5c246e8e5860b58388d956f049450c4c",
+    "url": "/template.git/static/media/svg-5.5c246e8e.svg"
+  },
+  {
+    "revision": "4491c8c98aa7f5545754c74367b65225",
+    "url": "/template.git/static/media/svg-7.4491c8c9.svg"
+  },
+  {
+    "revision": "c78a1bf247367c615eff109430742e97",
+    "url": "/template.git/static/media/svg-8.c78a1bf2.svg"
+  }
+]);
